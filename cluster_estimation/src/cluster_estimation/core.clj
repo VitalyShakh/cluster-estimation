353a776503baf30cb43394f9e9e56facb5473d0e
Vitaly Shakh <v.shah@itransition.com>
2014-11-03 11:31:47 +0300

    Implemented logic of potentials update.

--- cluster_estimation/src/cluster_estimation/core.clj
+++ cluster_estimation/src/cluster_estimation/core.clj
@@ -14,11 +14,18 @@
 (defn get-distance [p1 p2] ;drop first element because first parameter is object id!
   (Math/sqrt(reduce + (drop 1 (map get-square-of-diff p1 p2)))))
 
+;gets potetial between two points
+(defn get-points-potential [p1 p2 coef]
+  (Math/exp (- (* coef (Math/pow (get-distance p1 p2) 2)))))
+
 (defn get-potential [data p alpha]
-  (list (reduce + (map #(Math/exp (- (* alpha (Math/pow (get-distance % p) 2)))) data)) p))
+  (list (reduce + (map #(get-points-potential % p alpha) data)) p))
 
 (defn get-potentials [data alpha]
   (map #(get-potential data % alpha) data))
+
+(defn update-potentials [potentials core beta]
+  (map #(vector (- (first %) (* (first core) (get-points-potential (last %) (last core) beta))) (last %)) potentials))
 
 (defn get-cluster-cores [data]
   (let [radius-a 5
@@ -27,8 +34,15 @@
         beta (/ 4 (Math/pow radius-b 2))
         upper-threshold 0.5
         lower-threshold 0.15
-        potentials (get-potentials data alpha)]
-  (last (apply max-key first potentials))))
+        potentials (get-potentials data alpha)
+        first-core (apply max-key first potentials)
+        cores (atom[(last first-core)])]
+    ;(loop [potentials (update-potentials(potentials first-core beta))
+           ;new-core (apply max-key first potentials)]
+        ;(swap! cores conj (last new-core))
+        ;()
+        @cores;)
+  ))
 
 (defn read-file [file]
   (def data (atom []))
@@ -36,6 +50,8 @@
     (doseq [line (line-seq rdr)]
       (if (not= line "")
         (swap! data conj (parse-parameters line)))))
-  (println (get-cluster-cores @data)))
+  (println (string/join "\n" (map str (get-cluster-cores @data)))))
 
 ;(read-file "resources/glass.data.txt")
+
+;(update-potentials [[14 [1 0 0]] [28 [2 3 4]]] [28 [1 3 4]] 0.07111)
